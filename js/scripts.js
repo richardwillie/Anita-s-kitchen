d0cf427654b5bc2e65ea485c0f3ee6cf101863f8
richard willie <richardwillie.w@gmail.com>
2018-02-19 15:49:06 +0300

    add scripts.js functions

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("Nav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("Nav").style.width = "0%";
+}
